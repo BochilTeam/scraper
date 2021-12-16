e6f15e3f7f4bdac7b21d018c71f8f15a93a93da1
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2021-12-16 10:03:59 +0700

    menambahkan waktu timeout

--- test/social-media.js
+++ test/social-media.js
@@ -55,7 +55,6 @@ describe('Social media', () => {
         })
 
         // it('Instagram Story', function (done) {
-        //     this.timeout(20 * 1000)
         //     instagramStory('freefirebgid').then(res => {
         //         expect(res).to.be.an('array')
         //         res.forEach(({ thumbnail, isVideo, url }) => {
@@ -70,7 +69,6 @@ describe('Social media', () => {
     })
 
     describe('Facebook (Metaverse :V)', function () {
-        this.timeout(20 * 1000)
         it('Facebook Downloader', done => {
             facebookdl('https://fb.watch/9WktuN9j-z/').then(res => {
                 expect(res).to.be.an('object')
