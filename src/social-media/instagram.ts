7c19c78ec1c4239b6cece91950f3ec2f5c517aac
BochilGaming <79433517+BochilGaming@users.noreply.github.com>
2022-02-22 08:24:50 +0700

    remove debug

--- src/social-media/instagram.ts
+++ src/social-media/instagram.ts
@@ -33,7 +33,6 @@ export async function instagramdl(url: string): Promise<InstagramDownloader[]> {
 		.split('decodeURIComponent(escape(r))}(')[1]
 		?.split(',').map(v => v.replace(/^"/, '')
 			.replace(/"$/, '').trim());
-	console.log(params)
 	if (!Array.isArray(params) || params.length !== 6) throw new ScraperError(`Can't parse encode params!\n${data}`)
 	const decode = decodeSnapApp(...params)
 	const html = decode?.split('("div_download").innerHTML = "')?.[1]
